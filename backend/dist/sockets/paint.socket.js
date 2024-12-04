2f0bd54e52adff6bd96ed6a2e6dc2230a3ab6582
Naomi <165420714+naomiTak@users.noreply.github.com>
2024-12-03 18:06:05 -0700

    Revert "add backend"

--- backend/dist/sockets/paint.socket.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//import { Paint } from '../models/chat.model';
-const setupPaintSocket = (io) => {
-};
-exports.default = setupPaintSocket;
