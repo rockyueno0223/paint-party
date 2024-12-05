ea445dd206cc6d657dc683eac1714f1903a281cc
Nao Gobaru <take57bamboo@gmail.com>
2024-12-05 15:34:42 -0700

    add

--- backend/dist/controllers/canvas.controller.js
+++ backend/dist/controllers/canvas.controller.js
@@ -12,25 +12,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const user_model_1 = __importDefault(require("../models/user.model"));
 const canvas_model_1 = require("../models/canvas.model");
 const canvasUser_model_1 = require("../models/canvasUser.model");
 const cloudinary_util_1 = require("../utils/cloudinary.util");
 const path_1 = __importDefault(require("path"));
 // Get all chats
 const getCanvasHistory = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { userId } = req.body;
+    const { email } = req.body;
     try {
         //const canvasUsers = await CanvasUser.find({ userId: userId }).populate('canvasId')
-        const canvasUsers = yield canvasUser_model_1.CanvasUser.find({ userId });
-        const canvasIds = canvasUsers.map(cu => cu.canvasId);
-        const canvases = yield canvas_model_1.Canvas.find({ _id: { $in: canvasIds } });
-        const canvasHistory = canvases.map(canvas => ({
-            canvasName: canvas.canvasName,
-            canvasURL: canvas.canvasURL
-        }));
-        console.log(canvasHistory);
-        //res.status(200).json(canvasUsers)
-        res.status(200).json({ history: canvasHistory, success: true });
+        // const user: ICanvasUser[] = await User.find(
+        //     { email: email },
+        //     { _id: 1 }    
+        // );
+        // const UserId = user[0]._id
+        // console.log(UserId)
+        // const canvasUsers: ICanvasUser[] = await CanvasUser.find({ UserId });
+        const user = yield user_model_1.default.find({ email: email }, { _id: 1 });
+        if (user.length > 0) {
+            const userId = user[0]._id;
+            const canvasUsers = yield canvasUser_model_1.CanvasUser.find({ userId });
+            const canvasIds = canvasUsers.map(cu => cu.canvasId);
+            const canvases = yield canvas_model_1.Canvas.find({ _id: { $in: canvasIds } });
+            const canvasHistory = canvases.map(canvas => ({
+                canvasName: canvas.canvasName,
+                canvasURL: canvas.canvasURL
+            }));
+            console.log(canvasHistory);
+            res.status(200).json({ history: canvasHistory, success: true });
+        }
+        else {
+            res.status(500).json({ success: false, message: 'User not found' });
+        }
     }
     catch (error) {
         res.status(500).json({ success: false, message: 'userId is invalid' });
@@ -57,26 +71,35 @@ const addProperty = (canvasName) => __awaiter(void 0, void 0, void 0, function* 
     // }
 });
 const saveCanvas = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { userId, canvasName } = req.body;
+    const { canvasName, imageUrl, email } = req.body;
     try {
-        const filePath = path_1.default.join(__dirname, `../../uploads/${canvasName}.png`);
+        //const filePath = path.join(__dirname, `../../uploads/${canvasName}.png`);
+        const filePath = imageUrl;
+        //console.log(filePath)
         const canvasURL = yield (0, cloudinary_util_1.uploadImage)(filePath, "paint-party");
         const canvas = new canvas_model_1.Canvas({ canvasName, canvasURL });
         const savedCanvas = yield canvas.save();
         const canvasIdString = savedCanvas._id.toString();
-        //console.log('New user ID:', canvasIdString);
-        const newCanvasUser = new canvasUser_model_1.CanvasUser({
-            canvasId: canvas._id,
-            userId: userId
-        });
-        yield newCanvasUser.save();
+        const user = yield user_model_1.default.find({ email: email }, { _id: 1 });
+        if (user.length > 0) {
+            const userId = user[0]._id;
+            //console.log(userId)
+            const newCanvasUser = new canvasUser_model_1.CanvasUser({
+                canvasId: canvasIdString,
+                userId: userId
+            });
+            yield newCanvasUser.save();
+            res.status(201).json({ user: userId, success: true });
+        }
+        else {
+            res.status(500).json({ success: false, message: 'userId is invalid' });
+        }
         //const canvasUser = new CanvasUser({ canvasIdString, userId });
         //await canvasUser.save();
-        res.status(201).json({ user: userId, success: true });
     }
     catch (error) {
         console.error(error);
-        res.status(500).json({ success: false, message: error });
+        res.status(500).json({ success: false, message: "Image saving failed" });
     }
 });
 exports.default = {
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -28,7 +28,7 @@ app.use('/api/auth', authRouter);
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:4321', // Your frontend url here (Astro, React, vanilla HTML)
+        origin: 'http://localhost:5173', // Your frontend url here (Astro, React, vanilla HTML)
         methods: ["GET", "POST"]
     },
 });
