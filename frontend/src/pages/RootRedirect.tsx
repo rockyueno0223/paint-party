c7284c508816b78d88d3a07a18f8360a097591e5
Hiroki Ueno <rockyueno@gmail.com>
2024-12-05 16:57:01 -0800

    created Root redirect and Private Route pages

---
+++ frontend/src/pages/RootRedirect.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+export const RootRedirect = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/api/auth/check-auth`,
+          {
+            credentials: "include",
+          }
+        );
+        const data = await res.json();
+        if (res.ok && data.success) {
+          navigate('/dashboard');
+        } else {
+          navigate('/signin');
+        }
+      } catch (error) {
+        console.error(error);
+        navigate('/signin');
+      }
+    }
+    checkAuth();
+  }, [navigate]);
+
+  return null
+}
